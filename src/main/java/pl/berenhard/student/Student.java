460b46a7b5e781c0670d15e5d5cb97e4095e4b38
s461995 <kamilberenhard@wp.pl>
2022-01-13 21:37:09 +0100

    Adding students entity

--- src/main/java/pl/berenhard/student/Student.java
+++ src/main/java/pl/berenhard/student/Student.java
@@ -1,4 +1,68 @@
 package pl.berenhard.student;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private int indexNumber;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String degreeCourse;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(int indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDegreeCourse() {
+        return degreeCourse;
+    }
+
+    public void setDegreeCourse(String degreeCourse) {
+        this.degreeCourse = degreeCourse;
+    }
 }
---
+++ src/main/java/pl/berenhard/student/StudentRepository.java
@@ -0,0 +1,4 @@
+package pl.berenhard.student;
+
+public interface StudentRepository {
+}

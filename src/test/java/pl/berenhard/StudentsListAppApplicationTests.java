7254cd064a30bb208aa252d94f5328679102ca01
s461995 <kamilberenhard@wp.pl>
2022-01-13 17:45:15 +0100

    creating repository

---
+++ src/test/java/pl/berenhard/StudentsListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.berenhard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

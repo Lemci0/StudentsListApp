dc41967cf372dbd181b584b627234abb3207ab70
s461995 <kamilberenhard@wp.pl>
2022-01-13 21:26:23 +0100

    Adding bootstrap

--- src/main/java/pl/berenhard/StudentsListAppApplication.java
+++ src/main/java/pl/berenhard/StudentsListAppApplication.java
@@ -1,9 +1,15 @@
 package pl.berenhard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class StudentsListAppApplication {
 
     public static void main(String[] args) {

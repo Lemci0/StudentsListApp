57d14c2d79b06aaf9e9bf6754975250013c39f9d
s461995 <kamilberenhard@wp.pl>
2022-01-13 19:55:45 +0100

    Adding homepage 1

--- src/main/java/pl/berenhard/MainController.java
+++ src/main/java/pl/berenhard/MainController.java
@@ -1,4 +1,13 @@
 package pl.berenhard;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("")
+    public String showHomePage() {
+        return "index";
+    }
 }
